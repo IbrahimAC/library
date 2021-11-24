c4e13134348c34b833eb98d75f228d9c6c5137c1
IbrahimAC <ibrahimchowdhury.notwork@gmail.com>
2021-11-24 12:14:43 +0000

    rubocop fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,12 @@ require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   [
-  SimpleCov::Formatter::Console
-# Want a nice code coverage website? 
-#Uncomment this next line!
-# SimpleCov::Formatter::HTMLFormatter
- ])
+    SimpleCov::Formatter::Console
+    # Want a nice code coverage website?
+    # Uncomment this next line!
+    # SimpleCov::Formatter::HTMLFormatter
+  ]
+)
 SimpleCov.start
 
 RSpec.configure do |config|
