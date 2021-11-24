1f1c80f1a7aefec971af417de9ebaa6cb444152e
IbrahimAC <ibrahimchowdhury.notwork@gmail.com>
2021-11-24 12:14:13 +0000

    'changes'

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 ruby '3.0.2'
-# gem "rails"
 
 group :test do
   gem 'simplecov', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,11 @@ require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   [
-    SimpleCov::Formatter::Console
-    # Want a nice code coverage website?
-    # Uncomment this next line!
-    # SimpleCov::Formatter::HTMLFormatter
-  ]
-)
+  SimpleCov::Formatter::Console
+# Want a nice code coverage website? 
+#Uncomment this next line!
+# SimpleCov::Formatter::HTMLFormatter
+ ])
 SimpleCov.start
 
 RSpec.configure do |config|
